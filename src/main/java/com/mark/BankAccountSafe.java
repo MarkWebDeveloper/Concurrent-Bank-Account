b7883fd19e4fb2c2ff5b9900e232788ea00eea10
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-08-22 13:22:20 +0200

    atomic increment implemented

--- src/main/java/com/mark/BankAccountSafe.java
+++ src/main/java/com/mark/BankAccountSafe.java
@@ -2,31 +2,34 @@ package com.mark;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class BankAccountSafe {
-    private double balance;
-    private int numberOfTransactions;
 
-    public BankAccountSafe(double balance) {
+    private double balance;
+    private AtomicInteger numberOfTransactions;
+
+    public BankAccountSafe(double balance, AtomicInteger numberOfTransactions) {
         this.balance = balance;
+        this.numberOfTransactions = numberOfTransactions;
     }
 
     public void deposit(double amount) {
         balance += amount;
         System.out.println("The current balance: " + balance);
-        System.out.println("The current num of transactions: " + ++numberOfTransactions);
+        System.out.println("The current num of transactions: " + numberOfTransactions.incrementAndGet());
     }
 
     public void withdraw(double amount) {
         balance -= amount;
         System.out.println("The current balance: " + balance);
-        System.out.println("The current num of transactions: " + ++numberOfTransactions);
+        System.out.println("The current num of transactions: " + numberOfTransactions.incrementAndGet());
     }
 
     public static void main(String[] args) {
         ExecutorService service = Executors.newFixedThreadPool(20);
         try {
-            BankAccountSafe bankAccount = new BankAccountSafe(100);
+            BankAccountSafe bankAccount = new BankAccountSafe(100, new AtomicInteger(0));
             for (int i = 0; i < 5; i++) {
                 service.submit(() -> bankAccount.deposit(10));
             }
