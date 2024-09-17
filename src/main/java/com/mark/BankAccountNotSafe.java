6fb6d7d0fd182e60a1aade3fa19c83908da9e594
Mark Kil <144998902+MarkWebDeveloper@users.noreply.github.com>
2024-09-17 12:05:58 +0200

    version 2 is ready

--- src/main/java/com/mark/BankAccountNotSafe.java
+++ src/main/java/com/mark/BankAccountNotSafe.java
@@ -5,37 +5,33 @@ import java.util.concurrent.Executors;
 
 public class BankAccountNotSafe {
 
-    private double balance;
+    private long balance;
     private int numberOfTransactions;
 
-    public BankAccountNotSafe(double balance) {
+    public BankAccountNotSafe(long balance) {
         this.balance = balance;
     }
 
     public void deposit(double amount) {
         balance += amount;
-        System.out.println("The current balance: " + balance);
-        System.out.println("The current num of transactions: " + ++numberOfTransactions);
-    }
-
-    public void withdraw(double amount) {
-        balance -= amount;
-        System.out.println("The current balance: " + balance);
-        System.out.println("The current num of transactions: " + ++numberOfTransactions);
+        System.out.println("The current balance is " + balance + " and the current number of transactions is:" + ++numberOfTransactions);
     }
 
     public static void main(String[] args) {
-        ExecutorService service = Executors.newFixedThreadPool(20);
-        try {
-            BankAccountNotSafe bankAccount = new BankAccountNotSafe(100);
-            for (int i = 0; i < 5; i++) {
-                service.submit(() -> bankAccount.deposit(10));
-            }
-            for (int i = 0; i < 5; i++) {
-                service.submit(() -> bankAccount.withdraw(10));
-            }
-        } finally {
-            service.shutdown();
+        BankAccountNotSafe bankAccount = new BankAccountNotSafe(100);
+        ExecutorService service = Executors.newFixedThreadPool(100);
+        for (int i = 0; i < 1000; i++) {
+            service.submit(() -> {
+                try {
+                    Thread.sleep(10);
+                    bankAccount.deposit(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    service.shutdown();
+                }
+            });
+
         }
     }
 }
--- src/main/java/com/mark/BankAccountSafe.java
+++ src/main/java/com/mark/BankAccountSafe.java
@@ -3,60 +3,45 @@ package com.mark;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.Lock;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class BankAccountSafe {
 
-    private volatile double balance;
+    private AtomicLong balance;
     private AtomicInteger numberOfTransactions;
-    Lock lock = new ReentrantLock();
+    ReentrantLock lock = new ReentrantLock();
 
-    public BankAccountSafe(double balance, AtomicInteger numberOfTransactions) {
+    public BankAccountSafe(AtomicLong balance, AtomicInteger numberOfTransactions) {
         this.balance = balance;
         this.numberOfTransactions = numberOfTransactions;
     }
 
-    public void deposit(double amount) {
+    public void deposit(long amount) {
         try {
             lock.lock();
-            balance += amount;
-            System.out.println("The current balance: " + balance);
-            System.out.println("The current num of transactions: " + numberOfTransactions.incrementAndGet());
-            Thread.sleep(1000);
-        } catch(InterruptedException e){
-            e.printStackTrace();
-        }finally {
+            long newBalance = balance.addAndGet(amount);
+            System.out.println("The current balance is " + newBalance + " and the current number of transactions is:" + numberOfTransactions.incrementAndGet());
+        } finally {
             lock.unlock();
-        }
-    }
-
-    public void withdraw(double amount) {
-        try {
-            lock.lock();
-            balance -= amount;
-            System.out.println("The current balance: " + balance);
-            System.out.println("The current num of transactions: " + numberOfTransactions.incrementAndGet());
-            Thread.sleep(2000);
-        } catch(InterruptedException e){
-            e.printStackTrace();
-        }  finally {
-            lock.unlock();
-        }
+        } 
     }
 
     public static void main(String[] args) {
-        ExecutorService service = Executors.newFixedThreadPool(20);
-        BankAccountSafe bankAccount = new BankAccountSafe(100, new AtomicInteger(0));
-        try {
-            service.submit(() -> bankAccount.deposit(10));
-            service.submit(() -> bankAccount.deposit(20));
-            service.submit(() -> bankAccount.deposit(30));
-            service.submit(() -> bankAccount.withdraw(10));
-            service.submit(() -> bankAccount.withdraw(20));
-            service.submit(() -> bankAccount.withdraw(30));
-        } finally {
-            service.shutdown();
+        BankAccountSafe bankAccount = new BankAccountSafe(new AtomicLong(), new AtomicInteger());
+        ExecutorService service = Executors.newFixedThreadPool(100);
+        for (int i = 0; i < 1000; i++) {
+            service.submit(() -> {
+                try {
+                    Thread.sleep(10);
+                    bankAccount.deposit(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    service.shutdown();
+                }
+            });
+
         }
     }
 }
